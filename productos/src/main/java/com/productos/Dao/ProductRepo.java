65dd6c9ce999c8d8e744e3f342561717fd95f24d
Jdcamacaro <davidjcamacaro@gmail.com>
2024-01-24 02:55:56 -0400

    no message

--- productos/src/main/java/com/productos/Dao/ProductRepo.java
+++ productos/src/main/java/com/productos/Dao/ProductRepo.java
@@ -1,5 +1,6 @@
 package com.productos.Dao;
 
+import com.productos.Model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<ProductModel, Long> {
